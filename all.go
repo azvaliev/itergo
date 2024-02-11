95de6a31613574efa91580b4de543e8f72072ee3
Azat Valiev <azat@valievventures.com>
2024-02-10 19:24:30 -0600

    add `all` method

---
+++ all.go
@@ -0,0 +1,13 @@
+package itergo
+
+// All returns true if all elements in the collection satisfy the condition,
+// if any element does not satisfy the condition, it returns false.
+func All[T any](collection []T, condition func(item T, idx int) bool) bool {
+	for idx, item := range collection {
+		if !condition(item, idx) {
+			return false
+		}
+	}
+
+	return true
+}
---
+++ all_test.go
@@ -0,0 +1,62 @@
+package itergo
+
+import (
+	"math/big"
+	"testing"
+)
+
+func TestAllCorrectOrderAndIndex(t *testing.T) {
+	expectedSlice := []int{1, 2, 3, 4}
+	_ = All(expectedSlice, func(item int, idx int) bool {
+		if item != expectedSlice[idx] {
+			t.Errorf("Expected %d at position %d but got %d", expectedSlice[idx], idx, item)
+		}
+		return true
+	})
+}
+
+func TestAllEmptySlice(t *testing.T) {
+	_ = All([]int{}, func(item int, idx int) bool {
+		t.Error("Slice is empty, should not reach here")
+		return false
+	})
+}
+
+func TestAllOneFalse(t *testing.T) {
+	expectedSlice := []int{1, 2, 3, 4}
+	if All(expectedSlice, func(item int, idx int) bool {
+		return item != 3
+	}) {
+		t.Error("One of the items is not 3")
+	}
+}
+
+func TestAllTrue(t *testing.T) {
+	expectedSlice := []string{"a", "b", "c", "d"}
+
+	if !All(expectedSlice, func(item string, idx int) bool {
+		return true
+	}) {
+		t.Error("Should not reach here, all true")
+	}
+}
+
+func TestAllFalse(t *testing.T) {
+	expectedSlice := []string{"a", "b", "c", "d"}
+
+	if All(expectedSlice, func(item string, idx int) bool {
+		return false
+	}) {
+		t.Error("Should not reach here, all false")
+	}
+}
+
+func TestAllPrimeNumbers(t *testing.T) {
+	expectedSlice := []int{2, 3, 5, 7, 11, 13, 17, 19}
+
+	if !All(expectedSlice, func(item int, idx int) bool {
+		return big.NewInt(int64(item)).ProbablyPrime(0)
+	}) {
+		t.Error("Should not reach here, all prime numbers")
+	}
+}
